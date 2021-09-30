02092c59ab60001a337cb6896ebc99be50b3a7a9
Alexis Toranzo <alexistoranzoar@gmail.com>
2021-09-29 21:13:11 -0300

    Update env variables

--- firebase/admin.js
+++ firebase/admin.js
@@ -2,9 +2,10 @@ const admin = require("firebase-admin");
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
-!admin.apps.length && admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://twitter-be7ec.firebaseio.com'
-});
+!admin.apps.length &&
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  });
 
-export const firestore = admin.firestore()
+export const firestore = admin.firestore();
